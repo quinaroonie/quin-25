8918569dbdc0836c57fe37d54c3478d2fcd1a735
Phil B <philanab@gmail.com>
2019-10-07 15:40:31 -0400

    add solution to first part of function.py

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ def get_chatbot_response(message):
         return "What's up!"
     elif command == "add":
         num1, num2 = args.split()
-        return num1 + num2
+        return int(num1) + int(num2)
     elif command == "divide":
         num1, num2 = args.split()
         return num1 / num2
