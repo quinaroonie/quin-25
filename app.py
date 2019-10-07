8001e4022ff923f2562111dc92be0f9de2835f85
Phil B <philanab@gmail.com>
2019-10-07 15:43:14 -0400

    remove socket.io irrelevant code

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-import os, flask, flask_socketio
+import os, flask
 
 app = flask.Flask(__name__)
-socketio = flask_socketio.SocketIO(app)
 
 @app.route('/')
 def hello():
     return 'Hello, world!'
 
 if __name__ == '__main__':
-    socketio.run(
-        app,
+    app.run(
         host=os.getenv('IP', '0.0.0.0'),
         port=int(os.getenv('PORT', 8080)),
         debug=True
